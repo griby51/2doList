fc8e327405ba6df08b5b79d56ed44f6a4ff2795d
Grib <gritrepailgri@gmail.com>
2024-12-15 19:40:10 +0100

    add task_area

--- main.py
+++ main.py
@@ -14,10 +14,16 @@ with open("data.json", "r") as f:
 style = Style.from_dict({
     "frame": "bg:#040404 fg:#106cbc",
     "box": "bg:#040404 fg:#106cbc",
+
     "list-undone-unselected": "bg:#040404 fg:#0b4583",
     "list-undone-selected": "bg:#106cbc fg:#05224b",
     "list-done-unselected": "bg:#040404 fg:#0b4583 strike",
     "list-done-selected": "bg:#106cbc fg:#05224b strike",
+
+    "task-undone-unselected": "bg:#040404 fg:#0b4583",
+    "task-undone-selected": "bg:#106cbc fg:#05224b",
+    "task-done-unselected": "bg:#040404 fg:#0b4583 strike",
+    "task-done-selected": "bg:#106cbc fg:#05224b strike",
 })
 
 kb = KeyBindings()
@@ -27,42 +33,53 @@ class List:
         self.name = name
         self.done = done
         self.id = id
-        self.tasks = [self.Task(task["name"], task["done"]) for task in tasks]
+        self.tasks = [self.Task(task['name'], task['done'], i) for i, task in enumerate(tasks)]
 
     class Task:
         def __init__(self, name, done, index):
             self.name = name
             self.done = done
+            self.index = index
 
 all_list = []
-tasks_in_active_list = [] 
-
-for list in data["lists"]:
-    all_list.append(List(list["name"], list["done"], list["id"], list["tasks"]))
 
 
+for _list in data["lists"]:
+    all_list.append(List(_list["name"], _list["done"], _list["id"], _list["tasks"]))
 
-actual_selected_list_tasks = []
+if len(all_list) != 0:
+    global selected_list
+    selected_list = all_list[0]
+
+
 
 def updateListArea(done_style, not_done_style, selected_done_style, selected_not_done_style, selected=None):
     formatted_text = []
 
     for list in all_list:
         style = done_style if list.done else not_done_style
-        if list.id == selected:
+        if list.id == selected.id:
             style = selected_done_style if list.done else selected_not_done_style
         formatted_text.append((style, list.name + "\n"))  
     return FormattedTextControl(formatted_text)
 
-def updateTask():
-    global actual_selected_list_tasks
-    actual_selected_list_tasks = []
-    
+def updateTaskArea(done_style, not_done_style, selected_done_style, selected_not_done_style, selected_list=None):
+    if selected_list is None:
+        return FormattedTextControl([])  # Pas de liste sélectionnée
+
+    formatted_text = []
+
+    for task in selected_list.tasks:
+        style = done_style if task.done else not_done_style
+        formatted_text.append((style, task.name + "\n"))  # Afficher l'index
+
+    return FormattedTextControl(formatted_text)
+
 
 def addList(new_list):
     all_list.append(new_list)
 
-selected_list = None if len(all_list) == 0 else 0
+selected_list = None if len(all_list) == 0 else all_list[0]
 
 list_area = Window(content=updateListArea("class:list-done-unselected", "class:list-undone-unselected", "class:list-done-selected", "class:list-undone-selected", selected_list))
 list_frame = Frame(list_area, style="class:frame", title="Listes")
@@ -74,7 +91,7 @@ task_frame = Frame(task_area, style="class:frame", title="Tâches")
 input_area = TextArea(multiline=False)
 input_frame = Frame(input_area, style="class:frame")
 
-layout = Layout(HSplit([Box(list_frame,padding=1,style="class:box"), Box(task_frame,padding=1,style="class:box")]))
+layout = Layout(VSplit([Box(list_frame,padding=1,style="class:box"), Box(task_frame,padding=1,style="class:box")]))
 
 show_input_frame = False
 
@@ -87,13 +104,13 @@ def _(event):
 @kb.add("up")
 def _(event):
     global selected_list
-    selected_list = (selected_list - 1) % len(all_list)
+    selected_list.id = (selected_list.id - 1) % len(all_list)
     list_area.content = updateListArea("class:list-done-unselected", "class:list-undone-unselected", "class:list-done-selected", "class:list-undone-selected", selected_list)
 
 @kb.add("down")
 def _(event):
     global selected_list
-    selected_list = (selected_list + 1) % len(all_list)
+    selected_list.id = (selected_list.id + 1) % len(all_list)
     list_area.content = updateListArea("class:list-done-unselected", "class:list-undone-unselected", "class:list-done-selected", "class:list-undone-selected", selected_list)
 
 @kb.add("+")
